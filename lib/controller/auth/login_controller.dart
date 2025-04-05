2cc7c1d87b29e2d5bd7328a0435d363ebcf7471d
Abdalmalik salwan <alisolan8@gmail.com>
2025-04-05 22:35:32 +0300

    login logic
    *create login function
    *handling the errors and success
    *save session of user on shared prefrence

--- lib/controller/auth/login_controller.dart
+++ lib/controller/auth/login_controller.dart
@@ -3,111 +3,116 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:newhotelyemeni/controller/auth/onboadercontroller/onboarderScreen_controller.dart';
 import 'package:newhotelyemeni/core/class/statusRquest.dart';
+import 'package:newhotelyemeni/core/consttint/rout_name.dart';
+import 'package:newhotelyemeni/core/consttint/sharedrefernces_keys.dart';
 import 'package:newhotelyemeni/core/function/handling.dart';
+import 'package:newhotelyemeni/core/function/show_loading_dialog.dart';
+
 import 'package:newhotelyemeni/core/servesies/myserves.dart';
+import 'package:newhotelyemeni/core/sharedWidget/api_errors_dialog.dart';
+import 'package:newhotelyemeni/core/sharedWidget/loaging_dialog.dart';
+
 import 'package:newhotelyemeni/data/model/users_model.dart';
 import 'package:newhotelyemeni/data/sours/remout/auth/login_data.dart';
 
-abstract class LoginController extends GetxController{
- showPassword();
- login();
- forgetpassword();
+abstract class LoginController extends GetxController {
+  showPassword();
+  login(String email, String password);
+  forgetpassword();
 }
 
-class LoginControllerImp extends LoginController{
+class LoginControllerImp extends LoginController {
+  late TextEditingController email;
+  late TextEditingController password;
+  GlobalKey<FormState> formstate = GlobalKey();
+  LoginData loginData = LoginData(Get.find());
+  UserPreferences userPreferences = UserPreferences(Get.find());
+  StatusRquest statusRquest = StatusRquest.nune;
+  List<UserModels> datausers = [];
 
- late TextEditingController email;
- late TextEditingController password;
-GlobalKey<FormState>formstate=GlobalKey();
-LoginData data = LoginData(Get.find());
-Myserves myserves = Get.find();
-StatusRquest statusRquest= StatusRquest.nune;
-List<UserModels> datausers=[];
-
-
-bool ispassword = true;
+  bool ispassword = true;
   @override
   showPassword() {
-    ispassword= ispassword==true?false:true;
+    ispassword = ispassword == true ? false : true;
     update();
   }
 
+  @override
+  forgetpassword() {
+    Get.toNamed(RoutName.checkPassword);
+  }
 
- @override
- forgetpassword() {
-    Get.toNamed("/checkPassword");
- }
+  @override
+  login(String email, String password) async {
+    if (formstate.currentState!.validate()) {
+      statusRquest = StatusRquest.loading;
+      showLoadingDialog(statusRquest);
 
- @override
- login()async{
-   if(formstate.currentState!.validate()){
-     statusRquest = StatusRquest.loading;
-     update();
-     var response = await data.postData(email.text,password.text);
-     statusRquest = handling(response);
-     if(statusRquest == StatusRquest.success){
-       if(response['status'] == "success"){
-         List databoye = response['data'];
-         datausers.addAll(databoye.map((e) => UserModels.fromJson(e),));
-         myserves.sharedPreferences.setString("userId", datausers[0].usersId!);
-         myserves.sharedPreferences.setString("userName", datausers[0].usersName!);
-         myserves.sharedPreferences.setString("userPhone", datausers[0].usersPhone!);
-         myserves.sharedPreferences.setString("userEmail", datausers[0].usersEmail!);
-         myserves.sharedPreferences.setString("userPassword", datausers[0].usersPassword!);
-         myserves.sharedPreferences.setString("step", "1");
-         Get.offAllNamed("/HomeScreen");
-       }
-       else if(response['status'] == "must verifycode your email"){
-         Get.dialog(AlertDialog(
-           title:Text("Error"),
-           content: Text("must chick for your verifycode"),
-           actions: [
-             InkWell(
-               onTap: (){
-               },
-               child: Text("Verify"),
-             )
-           ],
-         ));
-       }
-       else{
-         print("============================must verifycode your email");
-         Get.dialog(AlertDialog(
-           title:Text("Error"),
-           content: Text("Not Find This Phone Or password"),
-         ));
-       }
-     }
-     else{
-       Get.dialog(AlertDialog(
-         title:Text("Error"),
-         content: Text("Content Wi_Fi"),
+      update();
+      try {
+        var resposn = await loginData.postData(email, password);
 
-       ));
-       statusRquest = StatusRquest.failure;
-     }
+        statusRquest = handling(resposn);
 
+        if (statusRquest == StatusRquest.success) {
+          Get.back();
+          userPreferences.saveUserData(resposn);
+          Get.offAllNamed(RoutName.homePage);
+        } else {
+          Get.back();
+          apiErorrsDialog(statusRquest);
+        }
+      } catch (e) {
+        Get.back();
+        print('error: $e');
+        Get.defaultDialog(
+            title: "failed".tr, middleText: 'enternal  failer'.tr);
+        statusRquest = StatusRquest.serverfailure;
+      } finally {
+        // showLoadingDialog();
+      }
+    }
+  }
 
-   }
- }
+  
 
-
-
- @override
+  @override
   void onInit() {
-email=TextEditingController();
-password=TextEditingController();
+    email = TextEditingController();
+    password = TextEditingController();
     super.onInit();
   }
 
-@override
+  @override
   void dispose() {
-email.dispose();
-password.dispose();
+    email.dispose();
+    password.dispose();
     super.dispose();
   }
+}
 
+class UserPreferences {
+  final MyServices myServices;
 
+  UserPreferences(this.myServices);
 
+  void saveUserData(Map<String, dynamic> userData) {
+    myServices.sharedPreferences
+        .setString(SharedreferncesKeys.id, '${userData['user']['id']}');
+    myServices.sharedPreferences
+        .setString(SharedreferncesKeys.token, userData['token']);
 
+    // myServices.sharedPreferences
+    //     .setString("access_token", userData['access_token']);
+    myServices.sharedPreferences
+        .setString(SharedreferncesKeys.name, userData['user']['name']);
+    myServices.sharedPreferences
+        .setString(SharedreferncesKeys.email, userData['user']['email']);
+    myServices.sharedPreferences.setString(
+        SharedreferncesKeys.phoneNumber, userData['user']['phone_number']);
+    myServices.sharedPreferences
+        .setString(SharedreferncesKeys.role, userData['user']['role']);
+
+    myServices.sharedPreferences.setString(SharedreferncesKeys.step, '2');
+  }
 }
