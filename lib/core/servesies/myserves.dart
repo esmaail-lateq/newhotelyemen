5da1fd307c04e4e42a2af84910420886b7336b72
Abdalmalik salwan <alisolan8@gmail.com>
2025-04-05 22:46:00 +0300

    name change

--- lib/core/servesies/myserves.dart
+++ lib/core/servesies/myserves.dart
@@ -1,17 +1,15 @@
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Myserves extends GetxService{
+class MyServices extends GetxService {
   late SharedPreferences sharedPreferences;
 
-  Future<Myserves> init()async{
-    sharedPreferences= await SharedPreferences.getInstance();
+  Future<MyServices> init() async {
+    sharedPreferences = await SharedPreferences.getInstance();
     return this;
-
   }
-
 }
-initialServers ()async{
-await Get.putAsync(()=> Myserves().init());
 
+initialServers() async {
+  await Get.putAsync(() => MyServices().init());
 }
