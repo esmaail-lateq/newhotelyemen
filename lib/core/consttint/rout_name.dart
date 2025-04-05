232b4ebc4942f6f5e831dfbdd1c29c35f8fea0c3
Abdalmalik salwan <alisolan8@gmail.com>
2025-04-05 22:42:16 +0300

    create routName and sharedPerfernceKeys

---
+++ lib/core/consttint/rout_name.dart
@@ -0,0 +1,14 @@
+class RoutName {
+  static String homePage = "/HomeScreen";
+  static String login = "/login";
+  static String signup = "/signup";
+  static String verifyEmail = "/verifyEmail";
+  static String successEmail = "/successEmail";
+  static String checkPassword = "/checkPassword";
+  static String verifyRepassword = "/verifyRepassword";
+  static String repassword = "/Repassword";
+  static String onboarder = "/onboarder";
+  static String settingscreen = "/settingscreen";
+  static String hotelsDetails = "/HotelsDetails";
+  static String aboutScreen = "/AboutScreen";
+}
---
+++ lib/core/consttint/sharedrefernces_keys.dart
@@ -0,0 +1,9 @@
+class SharedreferncesKeys {
+  static String name = 'name';
+  static String email = 'email';
+  static String id = 'id';
+  static String phoneNumber = 'phone_number';
+  static String role = 'role';
+  static String token = 'token';
+  static String step = 'step';
+}
