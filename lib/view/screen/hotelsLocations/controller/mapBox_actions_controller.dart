56009fd5855648e189a4e7a1d4a029e898caca68
Abdalmalik salwan <alisolan8@gmail.com>
2025-04-24 00:03:09 +0300

    improve the code

--- lib/view/screen/hotelsLocations/controller/mapBox_actions_controller.dart
+++ lib/view/screen/hotelsLocations/controller/mapBox_actions_controller.dart
@@ -1,29 +1,45 @@
 import 'dart:convert';
-import 'dart:ffi';
-import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' as map;
 import 'package:geolocator_platform_interface/src/models/position.dart' as pos;
-
+import 'package:turf/src/polyline.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:newhotelyemeni/core/class/statusRquest.dart';
+import 'package:newhotelyemeni/core/consttint/map_layer_constins.dart';
+import 'package:newhotelyemeni/core/function/handling.dart';
+import 'package:newhotelyemeni/core/function/string_to_list.dart';
 import 'package:newhotelyemeni/view/screen/hotelsLocations/controller/helper_class.dart';
 import 'package:newhotelyemeni/view/screen/hotelsLocations/controller/hotel_controller.dart';
 import 'package:newhotelyemeni/view/screen/hotelsLocations/data/get_rout_line_coordinates.dart';
+import 'package:newhotelyemeni/view/screen/hotelsLocations/view/widget/map_bottom_sheet.dart';
 
 class MapboxActionsController extends GetxController
     with GetSingleTickerProviderStateMixin {
   PointAnnotationManager? pointAnnotationManager;
   AnimationController? controller;
+  RoutData routData = RoutData(Get.find());
   late map.MapboxMap mapboxMapController;
+  Map? routDataReault;
   Animation<double>? animation;
+  Rx<StatusRquest> statusRquest = StatusRquest.nune.obs;
+  final defaultEdgeInsets =
+      MbxEdgeInsets(top: 100, left: 100, bottom: 100, right: 100);
 
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    controller?.dispose();
+    super.onClose();
   }
 
   void addLayerAndSource(
@@ -42,148 +58,97 @@ class MapboxActionsController extends GetxController
             [],
             null);
 
-        var data = {
-          'type': 'geojson',
-          'data': {
-            'type': 'FeatureCollection',
-            'features': hotels
-                .map(
-                  (e) => e.toGeoJson(),
-                )
-                .toList()
-          }
-        };
-
-        mapboxMap?.style.addStyleSource("point_source", json.encode(data));
+        mapboxMap.style.addStyleSource(
+            "point_source", json.encode(iconHotelsData(hotels)));
       }
     });
 
     mapboxMap.style.styleLayerExists("point_layer").then((value) async {
       if (!value) {
-        var layer = {
-          "id": "point_layer",
-          "type": "symbol",
-          "source": "point_source"
-        };
-        mapboxMap.style.addStyleLayer(json.encode(layer), null);
-        var properties = {
-          "icon-image": "icon",
-          "icon-opacity": 1.0,
-          "icon-size": [
-            "interpolate",
-            ["linear"],
-            ["zoom"],
-            8,
-            0.0,
-            10,
-            0.02,
-            12,
-            0.03,
-            13,
-            0.048,
-          ],
-          "icon-allow-overlap": true,
-          "icon-color": "RED"
-        };
-        mapboxMap?.style
-            .setStyleLayerProperties("point_layer", json.encode(properties));
+        mapboxMap.style.addStyleLayer(json.encode(pointLayer), null);
+
+        mapboxMap.style
+            .setStyleLayerProperties("point_layer", json.encode(mapIconStyle));
       }
     });
     mapboxMap.style.styleLayerExists("hotels-text-layer").then((value) async {
       if (!value) {
-        var layer = {
-          "id": "hotels-text-layer",
-          "type": "symbol",
-          "source": "point_source"
-        };
-        mapboxMap?.style.addStyleLayer(json.encode(layer), null);
-        var properties = {
-          // "minZoom": 10,
-          "layout": {
-            "text-field": ["get", "name"],
-            "text-size": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              8,
-              0.0,
-              10,
-              8,
-              12,
-              10,
-            ],
-            "text-offset": [0, 1.9],
-            // "text-visibility": [
-            //   "step",
-            //   ["zoom"],
-            //   8,
-            //   "none",
-            //   13,
-            //   "visible"
-            // ]
-          },
-          "paint": {
-            "text-color": "#e74c3c",
-            // "text-halo-color": "#ffffff",
-            "text-halo-width": 1.5
-          }
-        };
-        mapboxMap?.style.setStyleLayerProperties(
-            "hotels-text-layer", json.encode(properties));
+        mapboxMap.style.addStyleLayer(json.encode(textHotelLayer), null);
+
+        mapboxMap.style.setStyleLayerProperties(
+            "hotels-text-layer", json.encode(textHotelStyle));
       }
     });
+
+    addClickAction();
+  }
+
+  addClickAction() {
     var tapInteractionBuggy = TapInteraction(
-        FeaturesetDescriptor(layerId: "point_layer"), (features, point) {
-      Get.bottomSheet(_buildHotelDetailsCard(
-        features.properties['name'].toString(),
-        features.properties['description'].toString(),
-        () async {
-          final start = await mapboxMapController.style.getPuckPosition();
-          print(features.properties['longitude']);
+        FeaturesetDescriptor(layerId: "point_layer"), (features, point) async {
+      statusRquest.value = StatusRquest.loading;
+      showHotelBottomSheet(
+          features.properties['name'].toString(),
+          features.properties['description'].toString(),
+          stringToList(features.properties['images'].toString()));
 
-          final coordinates = await fetchRouteCoordinates(
-              start!,
-              Position(
-                  double.parse(features.properties['longitude'].toString()),
-                  double.parse(features.properties['latitude'].toString())));
-          // await fetchRouteCoordinates(start!, point.point.coordinates);
+      final start = await mapboxMapController.style.getPuckPosition();
 
-          drawRouteLowLevel(coordinates);
-          Get.back();
-        },
-      ));
-      print('${features.properties['name'].toString()}');
+      await fetchRoutDataCoordinates(
+          start!,
+          Position(double.parse(features.properties['longitude'].toString()),
+              double.parse(features.properties['latitude'].toString())));
+
+      // Get.bottomSheet(_buildHotelDetailsCard(
+      //   features.properties['name'].toString(),
+      //   features.properties['description'].toString(),
+      //   () async {
+      // final start = await mapboxMapController.style.getPuckPosition();
+      // print(features.properties['images']);
+
+      // var coordinates = await routData.fetchRouteCoordinates(
+      //     start!,
+      //     Position(
+      //         double.parse(features.properties['longitude'].toString()),
+      //         double.parse(features.properties['latitude'].toString())));
+      //     // await fetchRouteCoordinates(start!, point.point.coordinates);
+
+      //     // drawRouteLowLevel(coordinates);
+      //     Get.back();
+      //   },
+      // ));
+      if (kDebugMode) {
+        print('${features.properties['name'].toString()}');
+      }
     }, radius: 100);
 
-    mapboxMap.addInteraction(tapInteractionBuggy,
+    mapboxMapController.addInteraction(tapInteractionBuggy,
         interactionID: "tap_interaction_buggy");
   }
 
   void addRouteLineLayerAndSource() async {
-    await mapboxMapController.style.addLayer(LineLayer(
-      id: 'layer',
-      sourceId: 'source',
-      lineCap: LineCap.ROUND,
-      lineJoin: LineJoin.ROUND,
-      lineBlur: 1.0,
-      lineColor: const Color.fromARGB(255, 64, 169, 255).value,
-      // lineDasharray: [1.0, 2.0],
-      lineWidth: 5.0,
-      // draw layer with gradient
-      // lineGradientExpression: [
-      //   "interpolate",
-      //   ["linear"],
-      //   ["line-progress"],
-      //   0.0,
-      //   ["rgb", 255, 255, 255],
-      //   0.4,
-      //   ["rgb", 255, 255, 255],
-      //   1.0,
-      //   ["rgb", 0, 0, 255]
-      // ],
-    ));
+    await mapboxMapController.style.addLayer(lineLayerStyle);
     mapboxMapController.style
         .addSource(GeoJsonSource(id: "source", lineMetrics: true));
+  }
+
+  fetchRoutDataCoordinates(Position start, Position end) async {
+    statusRquest.value = StatusRquest.loading;
+
+    try {
+      var resposn = await routData.fetchRouteCoordinates(start, end);
+
+      var statusRquest1 = handling(resposn);
+
+      if (statusRquest1 == StatusRquest.success) {
+        routDataReault = resposn;
+        statusRquest.value = StatusRquest.success;
+      } else {
+        statusRquest.value = StatusRquest.failure;
+      }
+    } catch (e) {
+      statusRquest.value = StatusRquest.serverfailure;
+    }
   }
 
   void findNearstHotel(
@@ -194,122 +159,59 @@ class MapboxActionsController extends GetxController
         hotels: hotels);
 
     if (result != null) {
-      final coordinates = await fetchRouteCoordinates(
+      // var coordinates = await routData.fetchRouteCoordinates(
+      //     Position(userPosition.longitude, userPosition.latitude),
+      //     Position(result.longitude, result.latitude));
+      statusRquest.value = StatusRquest.loading;
+
+      changeCameraPosition(Position(result.longitude, result.latitude));
+      showHotelBottomSheet(result.name, result.description, result.images);
+
+      await fetchRoutDataCoordinates(
           Position(userPosition.longitude, userPosition.latitude),
           Position(result.longitude, result.latitude));
+      // print(coordinates);
 
-      drawRouteLowLevel(coordinates);
+      // drawRouteLowLevel(coordinates);
     }
+  }
+
+  drawRoutLineAndChangeCameraPostition(String coordint) async {
+    final position = await mapboxMapController.style.getPuckPosition();
+
+    Get.back();
+    changeCameraPosition(position!);
+    drawRouteLowLevel(Polyline.decode(coordint));
   }
 
   drawRouteLowLevel(List<Position> polyline) async {
+  
     final line = LineString(coordinates: polyline);
     final source = await mapboxMapController.style.getSource("source");
     (source as GeoJsonSource).updateGeoJSON(json.encode(line));
-    mapboxMapController.style
-        .setStyleLayerProperty("layer", "line-trim-offset", [1.0, 1.0]);
 
-    // // animate layer to reveal it from start to end
-    // controller?.stop();
-    // controller =
-    //     AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    // animation = Tween<double>(begin: 0, end: 1.0).animate(controller!)
-    //   ..addListener(() async {
-    //     // set the animated value of lineTrim and update the layer
-
-    //   });
-    // controller?.forward();
+    // animate layer to reveal it from start to end
+    controller?.stop();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 1.0).animate(controller!)
+      ..addListener(() async {
+        // set the animated value of lineTrim and update the layer
+        mapboxMapController.style.setStyleLayerProperty(
+            "layer", "line-trim-offset", [animation?.value, 1.0]);
+      });
+    controller?.forward();
   }
 
-  Widget _buildHotelDetailsCard(
-      String name, String decs, void Function()? onPressed) {
-    return Container(
-      width: 250,
-      height: 300,
-      child: Card(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                name,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
-              Text(decs),
-              SizedBox(height: 8),
-              ElevatedButton(
-                onPressed: () {
-                  // التنقل إلى صفحة تفاصيل الفندق
-                },
-                child: Text('عرض التفاصيل'),
-              ),
-              ElevatedButton(
-                onPressed: onPressed,
-                child: Text('الاتجاهات'),
-              ),
-            ],
-          ),
+  void changeCameraPosition(Position position) async {
+    mapboxMapController.flyTo(
+        CameraOptions(
+          center: Point(coordinates: position),
+          padding: defaultEdgeInsets,
+          zoom: 14,
         ),
-      ),
-    );
+        MapAnimationOptions(duration: 1200, startDelay: 0));
   }
+
+  
 }
-
-// // 2. دالة التحديث
-//   void _updateAnnotationSizes(double currentZoom) async {
-//     final newSize = calculateSizeBasedOnZoom(currentZoom);
-
-//     // final annotations = await pointAnnotationManager?.();
-
-//     // annotations?.forEach((annotation) {
-//     //   annotation.iconSize = newSize;
-//     //   pointAnnotationManager?.update(annotation);
-//     // });
-//   }
-
-double calculateSizeBasedOnZoom(double zoom) {
-  return zoom.clamp(10, 16).map(10.0, 16.0, 0.5, 1.5);
-}
-
-class AnnotationClickListener extends OnPointAnnotationClickListener {
-  final Function(HotelModel) onHotelSelected;
-  final RxList<HotelModel>? hotels;
-
-  AnnotationClickListener(this.onHotelSelected, this.hotels);
-
-  @override
-  void onPointAnnotationClick(PointAnnotation annotation) {
-    print("onAnnotationClick, id: ${annotation.id} ${annotation.textField}");
-    // final hotelId = annotation.data?['hotelId'];
-    // if (hotelId != null) {
-    //   final hotel = hotels.firstWhere((h) => h.id == hotelId);
-    //   onHotelSelected(hotel);
-    // }
-
-    for (var hotel in hotels!) {
-      if (hotel.name == annotation.textField) {
-        onHotelSelected(hotel);
-      }
-    }
-  }
-}
-
-extension on num {
-  double map(double fromMin, double fromMax, double toMin, double toMax) {
-    return toMin + ((this - fromMin) * (toMax - toMin)) / (fromMax - fromMin);
-  }
-}
-
-// 3. حساب الحجم بناءً على التكبير
-
-
-// إضافة امتداد للتحويل الخطي
-// extension Range on double {
-//   double map(double fromMin, double fromMax, double toMin, double toMax) {
-//     return toMin + ((this - fromMin) * (toMax - toMin)) / (fromMax - fromMin);
-//   }
-// }
-
-
--- lib/view/screen/hotelsLocations/data/get_rout_line_coordinates.dart
+++ lib/view/screen/hotelsLocations/data/get_rout_line_coordinates.dart
@@ -3,29 +3,50 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:newhotelyemeni/core/class/curd.dart';
 import 'package:turf/src/polyline.dart';
 
 const MAPBOX_DIRECTIONS_ENDPOINT =
     "https://api.mapbox.com/directions/v5/mapbox/driving/";
 
-Future<List<Position>> fetchRouteCoordinates(
-  Position start,
-  Position end,
-) async {
-  String? accessToken = await getAccessToken();
-  final response = await fetchDirectionRoute(start, end, accessToken ?? '');
-  Map<String, dynamic> route = jsonDecode(response.body);
-  return Polyline.decode(route['routes'][0]['geometry']);
+class RoutData {
+  CURD curd;
+  RoutData(this.curd);
+
+  fetchRouteCoordinates(
+    Position start,
+    Position end,
+  ) async {
+    String? accessToken = await getAccessToken();
+    var response = await curd.postData(
+        "$MAPBOX_DIRECTIONS_ENDPOINT${start.lng},${start.lat};${end.lng},${end.lat}?overview=full&access_token=${accessToken ?? ''}",
+        {},
+        {},
+        requestType: 'mapbox');
+
+    return response.fold((l) => l, (r) => r);
+  }
 }
+
+// Future<List<Position>> fetchRouteCoordinates(
+//   Position start,
+//   Position end,
+// ) async {
+//   String? accessToken = await getAccessToken();
+//   final response = await fetchDirectionRoute(start, end, accessToken ?? '');
+//   Map<String, dynamic> route = jsonDecode(response.body);
+//   print(route['routes'][0]);
+//   return Polyline.decode(route['routes'][0]['geometry']);
+// }
 
 getAccessToken() async {
   await dotenv.load(fileName: '.env');
   return dotenv.env['MAPBOX_ACCESS_TOKEN'];
 }
 
-Future<http.Response> fetchDirectionRoute(
-    Position start, Position end, String accessToken) async {
-  final uri = Uri.parse(
-      "$MAPBOX_DIRECTIONS_ENDPOINT${start.lng},${start.lat};${end.lng},${end.lat}?overview=full&access_token=$accessToken");
-  return http.get(uri);
-}
+// Future<http.Response> fetchDirectionRoute(
+//     Position start, Position end, String accessToken) async {
+//   final uri = Uri.parse(
+//       "$MAPBOX_DIRECTIONS_ENDPOINT${start.lng},${start.lat};${end.lng},${end.lat}?overview=full&access_token=$accessToken");
+//   return http.get(uri);
+// }
