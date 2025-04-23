104e848b74dc6706b4f412e0dfdd94bdd4db9ce2
Abdalmalik salwan <alisolan8@gmail.com>
2025-04-24 00:01:58 +0300

    add helper function

--- lib/view/screen/hotelsLocations/controller/helper_class.dart
+++ lib/view/screen/hotelsLocations/controller/helper_class.dart
@@ -7,13 +7,16 @@ import 'package:newhotelyemeni/view/screen/hotelsLocations/controller/hotel_cont
 class NearestPointFinder {
   static double _degToRad(double deg) => deg * pi / 180;
 
-  static double _calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+  static double _calculateDistance(
+      double lat1, double lon1, double lat2, double lon2) {
     const R = 6371; // نصف قطر الأرض بالكيلومتر
     final dLat = _degToRad(lat2 - lat1);
     final dLon = _degToRad(lon2 - lon1);
     final a = sin(dLat / 2) * sin(dLat / 2) +
-        cos(_degToRad(lat1)) * cos(_degToRad(lat2)) *
-            sin(dLon / 2) * sin(dLon / 2);
+        cos(_degToRad(lat1)) *
+            cos(_degToRad(lat2)) *
+            sin(dLon / 2) *
+            sin(dLon / 2);
     final c = 2 * atan2(sqrt(a), sqrt(1 - a));
     return R * c;
   }
@@ -27,7 +30,8 @@ class NearestPointFinder {
     double minDistance = double.infinity;
 
     for (var hotel in hotels) {
-      final distance = _calculateDistance(userLat, userLon, hotel.latitude, hotel.longitude);
+      final distance =
+          _calculateDistance(userLat, userLon, hotel.latitude, hotel.longitude);
       if (distance < minDistance) {
         minDistance = distance;
         nearest = hotel;
@@ -52,4 +56,40 @@ extension PuckPosition on StyleManager {
     }
     return Future.value(Position(location[1]!, location[0]!));
   }
+}
+
+String formatDistance(
+  double distanceKm,
+) {
+  String distanceText;
+
+
+  // المسافة
+  if (distanceKm < 1) {
+    distanceText = "${(distanceKm * 1000).round()} متر";
+  } else {
+    distanceText = "${distanceKm.toStringAsFixed(1)} كم";
+  }
+
+  return distanceText;
+
+}
+
+String formatTime(
+  double durationMinutes,
+) {
+  String timeText;
+  // الوقت
+  if (durationMinutes < 60) {
+    timeText = "${durationMinutes.round()} دقيقة";
+  } else {
+    int hours = durationMinutes ~/ 60;
+    int minutes = (durationMinutes % 60).round();
+    if (minutes == 0) {
+      timeText = "$hours ساعة";
+    } else {
+      timeText = "$hours ساعة و $minutes دقيقة";
+    }
+  }
+  return timeText;
 }
