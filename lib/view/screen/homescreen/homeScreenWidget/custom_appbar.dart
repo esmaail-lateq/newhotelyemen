416f4e4f4eb7498efee778a2d0776a9d4115d9d0
Abdalmalik salwan <alisolan8@gmail.com>
2025-04-24 00:00:42 +0300

    file import update

--- lib/view/screen/homescreen/homeScreenWidget/custom_appbar.dart
+++ lib/view/screen/homescreen/homeScreenWidget/custom_appbar.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:newhotelyemeni/core/consttint/colors.dart';
 
 import 'package:newhotelyemeni/view/screen/hotelsLocations/test/map_view.dart';
-import 'package:newhotelyemeni/view/screen/hotelsLocations/map_view_screen.dart';
+import 'package:newhotelyemeni/view/screen/hotelsLocations/view/map_view_screen.dart';
 import 'package:newhotelyemeni/view/widget/elevatedbuttons.dart';
 import 'package:newhotelyemeni/view/widget/fontandtext.dart';
 
