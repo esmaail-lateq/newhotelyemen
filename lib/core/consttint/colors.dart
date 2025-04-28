80737e72869a0944d0a92e53ac6574268e07e998
Abdalmalik salwan <alisolan8@gmail.com>
2025-04-28 22:54:17 +0300

    add colours and themes

--- lib/core/consttint/colors.dart
+++ lib/core/consttint/colors.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
-
-
-  static Color primaryColor=Color(0xFF143153);
-  static Color background=Colors.white10;
-  static Color text2=Colors.white;
-  static Color text1=Colors.cyanAccent;
-  static Color text3=Colors.cyan;
-
-
+class AppColors {
+  static const Color primaryColor = Color(0xFF143153);
+  static const Color secundColor = Color.fromARGB(255, 184, 216, 252);
+  static const Color theerdColor = Color.fromARGB(255, 224, 226, 227);
+  static const Color theerdColorThinker = Color.fromARGB(255, 161, 177, 185);
+  static const Color background = Colors.white10;
+  static const Color white = Colors.white;
+  static const Color gray = Colors.grey;
+  static const Color text2 = Colors.white;
+  static const Color text1 = Colors.cyanAccent;
+  static const Color text3 = Colors.cyan;
 }
---
+++ lib/core/consttint/text_tthems.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:newhotelyemeni/core/consttint/colors.dart';
+
+class TextThemsCustom {
+  static const TextStyle font22BlackBolde =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black);
+  static const TextStyle font22PrimaryColorBolde = TextStyle(
+      fontSize: 22, fontWeight: FontWeight.bold, color: AppColors.primaryColor);
+  static const TextStyle font12GrayColor =
+      TextStyle(fontSize: 14, color: AppColors.gray);
+}
